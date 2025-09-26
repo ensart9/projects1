1335fc8b37becaec237f2b385f3a68a29717acdd
ensar <149681706+ensart9@users.noreply.github.com>
2025-09-27 00:07:07 +0300

    Add files via upload

---
+++ basicCalculator.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+
+float calcMachine(float m,char operator,float n)
+{
+    switch(operator)
+    {
+        case '+':  return m+n;
+        case '-':  return m-n;
+        case '*': return m*n;
+
+        case '/':
+            if(m==0 && n==0){
+                printf("Indeterminate!");
+            }
+            else if(n==0){
+                printf("Undefined!");
+            }
+        return m/n;
+            
+        case '^':
+            if(m==0 && n==0)
+            {
+                printf("Undefined!");
+                return 0;
+            }
+        return pow(m,n);
+    } 
+}
+
+
+int main()
+{
+    float x,y;
+    char operation;
+
+    while(1){
+        printf("Please enter your operation: ");
+
+        if(scanf("%f %c %f",&x,&operation,&y)==3)
+        {
+                if(operation=='+' || operation=='-' || operation=='*' || operation=='/' || operation=='^'){
+                    calcMachine(x,operation,y);
+                    break;
+            }
+                else{
+                printf("Invalid operation! Please try again.\n");
+                }
+        }
+
+        else
+        {
+            printf("Invalid number input! Please enter again.\n");
+            while(getchar()!='\n');
+        }
+    }
+
+    float result=calcMachine(x,operation,y);
+    printf("%.2f",result);
+}
